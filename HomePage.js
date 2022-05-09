889fff138e114aa7333753bd68e6450be6c45508
Fady Milad <105245926+iiLuciferMS@users.noreply.github.com>
2022-05-09 22:41:36 +0200

    FadyMilad - Initial version

---
+++ HomePage.js
@@ -0,0 +1,9 @@
+﻿function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("temp1").style.marginRight = "61px";
+    document.getElementById("temp2").style.marginRight = "61px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
